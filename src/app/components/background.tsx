ea260d29a0108e9b9def2cad4a9342e3f187a4b1
Andy Vu <andyvu2026@u.northwestern.edu>
2025-12-15 15:19:36 +0800

    got background animated

--- src/app/components/background.tsx
+++ src/app/components/background.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import Image from 'next/image'
+import { Canvas } from '@react-three/fiber'
+import SphereMesh from './sphereMesh'
+import Stars from './stars'
+import ScrollCameraController from './scrollCameraController'
 
 export default function Background() {
 	return (
-		<div className="fixed inset-0 pointer-events-none z-[-10] overflow-hidden">
-			<Image
-				src="/mojave_background.jpg"
-				alt="Mojave background"
-				fill
-				className="object-cover"
-				priority
-			/>
+		<div className="fixed bg-black inset-0 pointer-events-none z-[-10] overflow-hidden">
+			<Canvas>
+				<ScrollCameraController />
+				<Stars />
+				<SphereMesh />
+				<ambientLight color="white" intensity={0.75} />
+				<directionalLight position={[0, 7, -7]} intensity={1} />
+			</Canvas>
 		</div>
 	)
 }
